37b823cc9dd0c905f49accfc91c62b74141752ef
Jmserge29 <Jmserge29@gmail.com>
2023-09-14 21:05:12 -0500

    Fixed errors in deploy (change 3)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,6 @@
 import "./globals.css";
 import Providers from "./Providers";
 
-export const metadata = {
-  title: "Nova-App",
-  description: "Creating application by XxJmsergexX",
-};
-
 export default function RootLayout({ children }) {
 
   return (
